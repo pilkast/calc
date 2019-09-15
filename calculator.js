1cff1e728d8a11b65c1b24d82068d582fffdbd76
Stanislav Shablinskiy <schablinski@hotmail.com>
2019-09-15 22:05:09 +0300

    replaced buttons to inputs

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
-/**
- * Created by Ноут on 09.03.2019.
- */
+function result(a, b) {
+    let pus = (a + b);
+    a = document.getElementById('a');
+    b = document.getElementById('b');
+
+}
+
+
+function plus() {
+    if ("a + b") {
+        let res = a + b;
+    }
+    else if ("a - b") {
+        let  res = a - b;
+    }
+}
