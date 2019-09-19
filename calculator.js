a8f8617cf65018893556e67fdd528ed110a42572
Stanislav Shablinskiy <schablinski@hotmail.com>
2019-09-19 17:15:49 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -14,3 +14,27 @@ function plus() {
         let  res = a - b;
     }
 }
+
+
+
+
+
+function result(a) {
+    document.getElementById('result').innerHTML += a;
+    document.getElementById("result").setAttribute("style",  "margin-left:"  +  950   + "px"  );
+
+}
+
+function result1() {
+    let aid;
+    let aResult;
+    let aLenght;
+    aid = document.getElementById("id1").innerHTML;
+    aResult = document.getElementById("result").innerHTML;
+    if (aResult === "0") {
+        document.getElementById("rezult").innerHTML = "1";
+    }
+    aLenght = aResult.length;
+}
+
+
