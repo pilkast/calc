7a71c7c283f9db1463a2fb14adac5d76283e5a6a
Stanislav Shablinskiy <schablinski@hotmail.com>
2019-09-20 16:00:46 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,40 +1,38 @@
-function result(a, b) {
-    let pus = (a + b);
-    a = document.getElementById('a');
-    b = document.getElementById('b');
-
-}
-
-
-function plus() {
-    if ("a + b") {
-        let res = a + b;
-    }
-    else if ("a - b") {
-        let  res = a - b;
-    }
-}
-
-
-
-
+let p1, p2, znach, res;
 
 function result(a) {
     document.getElementById('result').innerHTML += a;
-    document.getElementById("result").setAttribute("style",  "margin-left:"  +  950   + "px"  );
-
 }
-
-function result1() {
-    let aid;
-    let aResult;
-    let aLenght;
-    aid = document.getElementById("id1").innerHTML;
-    aResult = document.getElementById("result").innerHTML;
-    if (aResult === "0") {
-        document.getElementById("rezult").innerHTML = "1";
+function resultZnak(c) {
+    p1 = parseInt(document.getElementById('result').innerHTML);
+    document.getElementById('znak').innerHTML = c;
+    if ('p1' || p1 === null) {
+        document.getElementById('archiv').innerHTML = p1;
+        document.getElementById('result').innerHTML = '';
+}
+}
+function resultRavno() {
+    p2 = parseInt(document.getElementById('result').innerHTML);
+    znach = document.getElementById('znak').innerHTML;
+    if (znach === '+') {
+        res = p1 + p2;
+        }
+    if (znach === '-') {
+        res = p1 - p2;
+        console.log(res);
     }
-    aLenght = aResult.length;
+    if (znach === '*') {
+        res = p1 * p2;
+        console.log(res);
+    }
+    if (znach === '/') {
+        res = p1 / p2;
+        console.log(res);
+    }
+    document.getElementById('result').innerHTML = res;
 }
-
-
+function clear() {
+    document.getElementById('result').innerHTML = '0';
+        document.getElementById('archiv').innerHTML = '';
+            document.getElementById('znak').innerHTML = '';
+}
